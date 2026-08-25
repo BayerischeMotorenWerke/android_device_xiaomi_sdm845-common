3324546e0440fea42e182ff756f861acb6be5b19
BayerischeMotorenWerke <syaifulamri199@gmail.com>
2026-08-25 07:05:33 +0700

    sdm845-common: Fix double-space in config.fs user entries
    The slim_daemon, loc_launcher and xtwifi-client entries used
    "user:  AID_GPS" (two spaces) while every other entry uses a single
    space. Normalize to the "key: value" convention used everywhere else.
    
    Change-Id: Ic1d2e3f4a5b6c7d8e9f0a1b2c3d4e5f60718293a

--- config.fs
+++ config.fs
@@ -63,19 +63,19 @@ caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
 
 [vendor/bin/slim_daemon]
 mode: 0755
-user:  AID_GPS
+user: AID_GPS
 group: AID_GPS
 caps: NET_BIND_SERVICE
 
 [vendor/bin/loc_launcher]
 mode: 0755
-user:  AID_GPS
+user: AID_GPS
 group: AID_GPS
 caps: SETUID SETGID
 
 [vendor/bin/xtwifi-client]
 mode: 0755
-user:  AID_GPS
+user: AID_GPS
 group: AID_GPS
 caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
 
